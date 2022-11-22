8971a0568473fbbfa3a0b657c0bc57ee156ff9be
nidhal amara <amaraanidhall@gmail.com>
2022-11-23 00:01:54 +0100

    [UPDATE] ML Code updated .

--- PythonScripts/WebScrapper/WebScrapper.py
+++ PythonScripts/WebScrapper/WebScrapper.py
@@ -1,6 +1,11 @@
-from bs4 import BeautifulSoup
 import requests
-import time
-company=input()
-def collectDailyData():
-    return 0
+import csv
+CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=AE9U2AN0HTR4WKEN'
+
+with requests.Session() as s:
+    download = s.get(CSV_URL)
+    decoded_content = download.content.decode('utf-8')
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    my_list = list(cr)
+    for row in my_list:
+        print(row)
